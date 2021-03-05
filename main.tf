bcebe0bad538cae5e58bd58f450707a9b69e6071
satyam <satyam.gcs@gmail.com>
2021-03-05 14:50:09 +0530

    updated

--- main.tf
+++ main.tf
@@ -10,19 +10,63 @@ resource "aws_vpc" "vpc" {
   }
 }
 resource "aws_subnet" "public-subnet" {
-  vpc_id                          = aws_vpc.vpc.id
-  cidr_block                      = "10.0.1.0/24"
-  availability_zone               = "us-west-2a"
-  map_public_ip_on_launch         = true
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "my-public-subnet"
   }
 }
 resource "aws_subnet" "private-subnet" {
-  vpc_id                          = aws_vpc.vpc.id
-  cidr_block                      = "10.0.2.0/24"
-  availability_zone               = "us-west-2b"
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
   tags = {
     Name = "my-private-subnet"
   }
+}
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "my-ig"
+  }
+}
+resource "aws_eip" "nat" {
+  vpc              = true
+}
+resource "aws_nat_gateway" "nat-ig" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-subnet.id
+  tags = {
+    Name = "my-nat-ig"
+  }
+}
+resource "aws_route_table" "public-RT" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+  tags = {
+    Name = "my-public-rt"
+  }
+}
+resource "aws_route_table" "private-RT" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-ig.id
+  }
+  tags = {
+    Name = "my-private-rt"
+  }
+}
+resource "aws_route_table_association" "public-rt-ass" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-RT.id
+}
+resource "aws_route_table_association" "private-rt-ass" {
+  subnet_id      = aws_subnet.private-subnet.id
+  route_table_id = aws_route_table.private-RT.id
 }
